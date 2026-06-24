c270455e5dc8a50760026754c21367d5d74d3f49
harshparmar009 <hardinmartin09@gmail.com>
2026-06-24 16:34:18 +0530

    adding check in tab 2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -11,7 +11,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Photo Gallery</IonTitle>
+          <IonTitle>Photo Gallery Check</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
